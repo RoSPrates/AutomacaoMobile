0bd2b17a4709b914ed54e995b4b8e72e7530c2dc
Rodrigo Prates <rodrigo.prates@yaman.com.br>
2020-06-03 19:06:07 -0300

    subindo git ignore

--- src/test/java/br/com/cursoudemy/appium/install/Install.java
+++ src/test/java/br/com/cursoudemy/appium/install/Install.java
@@ -1,19 +1,12 @@
 package br.com.cursoudemy.appium.install;
 
 import br.com.cursoudemy.appium.enuns.Apk;
-import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.pagefactory.AndroidBy;
-import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.touch.offset.PointOption;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
@@ -43,14 +36,14 @@ public class Install {
         try {
             remoteUrl = new URL("http://localhost:4723/wd/hub");
             driver = new AndroidDriver(remoteUrl, capabilities);
-            wait = new WebDriverWait(driver,120);
+            wait = new WebDriverWait(driver, 120);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void login(){
+    public void login() {
 
     }
 }
